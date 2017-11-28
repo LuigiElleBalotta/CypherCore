47df7e6212274e109ebad4e48a00ec0a6651438d
Luigi <luigi.balotta@exelis.it>
2017-11-28 17:45:13 +0100

    Revert some things

--- Source/Game/Text/CreatureTextManager.cs
+++ Source/Game/Text/CreatureTextManager.cs
@@ -205,7 +205,7 @@ namespace Game
                 BroadcastTextRecord bct = CliDB.BroadcastTextStorage.LookupByKey(textEntry.BroadcastTextId);
                 if (bct != null)
                 {
-                    uint broadcastTextSoundId = bct.SoundID;
+                    uint broadcastTextSoundId = bct.SoundID[source.GetGender() == Gender.Female ? 1 : 0];
                     if (broadcastTextSoundId != 0)
                         finalSound = broadcastTextSoundId;
                 }
